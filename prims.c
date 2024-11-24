f860e80faf45b88198e8265fd5fa5383fe3dfccb
Sam-Wadmare <159400424+Sam-Wadmare@users.noreply.github.com>
2024-11-24 08:47:58 +0530

    Update prims.c

--- prims.c
+++ prims.c
@@ -1,100 +1,47 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#define INF 9999
 
-int n,cost[10][10];
+int n, cost[10][10];
 
-void prim() {
-int i,j,startVertex,endVertex;
-int k,nr[10],temp,minimumCost=0,tree[10][3];
-
-/*for the smallest edge */
-
-temp = cost[0][0];
-for(i=0;i<n;i++){
-    for(j=0;j<n;j++) {
-        if (temp>cost[i][j]) {
-            temp=cost[i][j];
-            startVertex=i;
-            endVertex=j;
+void prims() {
+    int visited[10]={0};
+    int totalcost=0;
+    visited[0] = 1;
+    printf("Edges in MST: \n");
+    int edge;
+    for(edge=0;edge<n-1;edge++){
+        int mincost=INF,u=-1,v=-1;
+        for(int i=0;i<n;i++) {
+            if(visited[i]) {
+                for(int j=0;j<n;j++) {
+                    if(!visited[j] && cost[i][j] < mincost) {
+                        mincost = cost[i][j];
+                        u=i;
+                        v=j;
+                    }
+                }
+            }
         }
+        if(u != -1 && v != -1) {
+            printf("%d to %d : %d \n",u,v,mincost);
+            totalcost+=mincost;
+            visited[v]=1;
+        }
+
     }
+    printf("total min cost is : %d",totalcost);
 }
+int main() {
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
 
-
-// now we have the smallest edge in the graph
-
-tree[0][0]=starttVertex;
-tree[0][1]=endVertex;
-tree[0][2]=temp;
-minimunCost=0;
-
-//now we have to find the minimum distance of each vertex from either startvertex or endvertex by initialising nr[] array
-
-for(i=0;i<n;i++) {
-    if(cost[i][startVertex]<cost[i][endVertex])
-        nr[i]=startVertex;
-    else
-        nr[i]=endVertex;
-}
-//top
-nr[startVertex]=100;
-nr[endVertex]=100;
-
-temp=99;
-for(i=0;i<n;i++) {
-    for(j=0;j<n;j++) {
-        if(nr[j]!=100 && cost[j][nr[j]] < temp) {
-            temp = cost[j][nr[j]];
-            k=j;
+    printf("Enter the cost adjacency matrix (use %d for no edge):\n", INF);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &cost[i][j]);
         }
     }
 
-    tree[i][0]=k;
-    tree[i][1]=nr[k];
-    tree[i][2]=cost[k][nr[k]];
-    minimumCost=minimumCost+cost[k][nr[k]];
-    nr[k]=100;
-
-    for(j=0;j<n;j++) {
-        if(nr[j]!= && cost[j][nr[j]] > cost[j][k])
-            nr[j]=k;
-    }
-    temp=99;
-}
-
-printf("the minimum spanning tree is : ");
-for(i=0;i<n-1;i++) {
-    for(j=0;j<3;j++)
-        printf("%d",tree[i][j]);
-    printf("\n");
-}
-printf("\n mincost:  %d ",minimumCost);
-
-}
-
-void main() {
-int i,j;
-clrsrc();
-
-printf("enter the no. of vertices");
-scanf("%d",&n);
-
-printf("Enter the cost of edges in matrix form");
-
-for(i=0;i<n;i++)
-    for(j=0;j<n;j++){
-    scanf("%d",&cost[i][j]);
-}
-printf("the matrix is : ");
-for(i=0;i<n;i++) {
-    for(j=0;j<n;j++) {
-        printf("%d",cost[i][j]);
-    }
-    printf("\n");
-}
-prim();
-getch();
-
-
-
+    prims();
+    return 0;
 }
